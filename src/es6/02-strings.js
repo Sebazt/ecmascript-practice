ad1c644a36ddf7eb615d8d6e23e4a652d8882f0a
Sebazt <juan.londono29@udea.edu.co>
2022-12-06 10:42:36 -0500

    :white_check_mark: add strings file

---
+++ src/es6/02-strings.js
@@ -0,0 +1,13 @@
+let hello = "Hi";
+let world = "World";
+
+//template literals
+let newFeature = `${hello} ${world}, this is the new feature for strings`
+console.log(newFeature);
+
+//multi-line strings
+
+let lorem = `${hello} this is a test
+and this is the continue of my test
+`
+console.log(lorem);
